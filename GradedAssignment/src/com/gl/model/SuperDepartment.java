76b9beda963b34d866b7d796d11f78a3b772455a
moulic3 <67384204+moulic3@users.noreply.github.com>
2023-06-18 23:31:15 +0530

    Add files via upload

---
+++ GradedAssignment/src/com/gl/model/SuperDepartment.java
@@ -0,0 +1,21 @@
+package com.gl.model;
+
+public class SuperDepartment {
+
+	public String departmentName() {
+		return "Super Department";
+	}
+
+	public String getTodaysWork() {
+		return "No Work as of now";
+	}
+
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+
+	public String isTodayHoliday() {
+		return "Today is not a holiday";
+	}
+
+}
